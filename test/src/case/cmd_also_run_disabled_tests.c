8e940e538f481f515b1056267e57f2a63538218d
qgymib <qgymib@gmail.com>
2023-01-28 22:44:09 +0800

    add test for `--test_also_run_disabled_tests`

---
+++ test/src/case/cmd_also_run_disabled_tests.c
@@ -0,0 +1,49 @@
+#include "test.h"
+#include <string.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Watchpoint
+///////////////////////////////////////////////////////////////////////////////
+
+typedef struct disabled_ctx
+{
+	int point_0;
+	int point_1;
+} disabled_ctx_t;
+
+static disabled_ctx_t g_disabled_ctx;
+
+TEST(disabled, DISABLED_point_0)
+{
+	g_disabled_ctx.point_0++;
+}
+
+TEST(disabled, disabled_point_1)
+{
+	g_disabled_ctx.point_1++;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Verify
+///////////////////////////////////////////////////////////////////////////////
+
+DEFINE_SETUP(disabled)
+{
+	memset(&g_disabled_ctx, 0, sizeof(g_disabled_ctx));
+}
+
+DEFINE_TEARDOWN(disabled)
+{
+}
+
+DEFINE_TEST_F(disabled, normal, "")
+{
+	CUTEST_PORTING_ASSERT(g_disabled_ctx.point_0 == 0);
+	CUTEST_PORTING_ASSERT(g_disabled_ctx.point_1 == 1);
+}
+
+DEFINE_TEST_F(disabled, run_all, "--test_also_run_disabled_tests")
+{
+	CUTEST_PORTING_ASSERT(g_disabled_ctx.point_0 == 1);
+	CUTEST_PORTING_ASSERT(g_disabled_ctx.point_1 == 1);
+}
